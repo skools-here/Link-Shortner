36209a669ac6bf15dd53a6927809b893b5397ec3
Sajal-Kulshreshtha <sajalkulshreshtha9@gmail.com>
2025-07-26 02:22:17 +0530

    deployment errors fix

--- r/route.ts
+++ r/route.ts
@@ -3,27 +3,24 @@ import dbConnect from "@/app/lib/connectDb";
 import Url from "@/app/models/urlSchema";
 
 export async function GET(req: NextRequest) {
+  // Get first query param key
   const urlId = req.nextUrl.searchParams.keys().next().value;
-    if (!urlId) {
+
+  if (!urlId) {
     return NextResponse.json({ status: 400, msg: "Missing URL ID" });
-    }
+  }
+
   try {
     await dbConnect();
-    const url = await Url.findOne({
-      urlId,
-    });
+    const url = await Url.findOne({ urlId });
+
     if (url) {
-      await Url.updateOne(
-        {
-          urlId,
-        },
-        { $inc: { clicks: 1 } }
-      );
+      await Url.updateOne({ urlId }, { $inc: { clicks: 1 } });
       return NextResponse.redirect(url.origUrl);
     } else {
-      return NextResponse.json({ status: 404, msg: fullUrl + " doesnt exist" });
+      return NextResponse.json({ status: 404, msg: `${urlId} does not exist` });
     }
   } catch (err) {
-    return NextResponse.json({ status: 404, error: err });
+    return NextResponse.json({ status: 500, error: err });
   }
 }
